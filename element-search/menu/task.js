1c0c09a3b1f6fed07d513e440081d07ff681a1d3
Alexander Burachkov <zicio@yandex.ru>
2021-04-26 22:29:24 +0400

    Popups and Menu Updated

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,12 @@
 'use strict'
-const link = document.getElementsByClassName('menu__link');
+const link = document.querySelectorAll('li a');
 for (let i = 0; i < link.length; i++) {
     link[i].onclick = () => {  
         //проверка на уже открытое меню при открытии другого
-        if (!link[i].closest('li').querySelector('ul.menu_sub').classList.contains('menu_active') && document.getElementsByClassName('menu_active').length !== 0) {
-            document.getElementsByClassName('menu_active')[0].classList.remove('menu_active');
-            link[i].closest('li').querySelector('ul.menu_sub').classList.add('menu_active');
+        if (!link[i].closest('li').querySelector('ul.menu_sub').classList.contains('menu_active') && link[i].closest('ul').querySelector('ul.menu_active')) {
+            link[i].closest('ul').querySelector('ul.menu_active').classList.remove('menu_active');
         }
-        //проверка на уже открытое меню при открытии этого же меню
-        else if (link[i].closest('li').querySelector('ul.menu_sub').classList.contains('menu_active')) {
-            link[i].closest('li').querySelector('ul.menu_sub').classList.remove('menu_active');
-        }
-        //открытие меню
-        else {
-            link[i].closest('li').querySelector('ul.menu_sub').classList.add('menu_active');
-        }
+        link[i].closest('li').querySelector('ul.menu_sub').classList.toggle('menu_active');
         return false;
     }
 }
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,15 @@
 'use strict'
-const main = document.getElementsByClassName('modal');
+const modal = document.getElementsByClassName('modal');
 const close = document.getElementsByClassName('modal__close_times');
-const success = document.getElementsByClassName('show-success');
-main[0].classList.add('modal_active');
+const success = document.querySelector('a.show-success');
+document.getElementById('modal_main').classList.add('modal_active');
 for (let i = 0; i < close.length; i++) {
     close[i].onclick = () => {
-        main[i].classList.remove('modal_active');
+        modal[i].classList.remove('modal_active');
     }
 }
-success[0].onclick = () => {
-    main[0].classList.remove('modal_active');
-    main[1].classList.add('modal_active');
+success.onclick = () => {
+    for (let i = 0; i < modal.length; i++) {
+        modal[i].classList.toggle('modal_active');
+    }
 }
