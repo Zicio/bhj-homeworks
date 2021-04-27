b786ba9eb1879c961e53ca873ed54a91b98b6555
Alexander Burachkov <zicio@yandex.ru>
2021-04-27 21:02:46 +0400

    element-search.: slider Solved

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,11 +1,14 @@
 'use strict'
 const arrows = document.getElementsByClassName('slider__arrow');
 const sliders = document.getElementsByClassName('slider__item');
+const dots = document.getElementsByClassName('slider__dot');
 let numberSlide = 0;
+dots[numberSlide].classList.add('slider__dot_active');
 
 for (let i = 0; i < arrows.length; i++) {
     arrows[i].onclick = () => {
         sliders[numberSlide].classList.remove('slider__item_active');
+        dots[numberSlide].classList.remove('slider__dot_active');
         if (arrows[i].classList.contains('slider__arrow_next')) {
             if (numberSlide === sliders.length - 1) {
             numberSlide -= sliders.length;
@@ -19,8 +22,18 @@ for (let i = 0; i < arrows.length; i++) {
         numberSlide -= 1;
         }
         sliders[numberSlide].classList.add('slider__item_active');
+        dots[numberSlide].classList.add('slider__dot_active');
     }
 }
 
-//arrows[1].closest('div.slider').querySelector('div.slider__item_active').classList.remove('slider__item_active');
-//arrows[1].closest('div.slider').querySelectorAll('div.slider__item').classList.toggle('slider__item_active');
+for (let i = 0; i < dots.length; i++) {
+    dots[i].onclick = () => {
+        if (i !== numberSlide) {
+            dots[numberSlide].classList.remove('slider__dot_active');
+            sliders[numberSlide].classList.remove('slider__item_active');
+            dots[i].classList.add('slider__dot_active');
+            sliders[i].classList.add('slider__item_active');
+            numberSlide = i;
+        }
+    }
+}
