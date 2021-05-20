51bb7d3172874daa110df9bc702a322737878868
Alexander Burachkov <zicio@yandex.ru>
2021-05-21 00:36:34 +0400

    async-requests: preloader Solved

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,16 +1,63 @@
 'use strict'
-const loader = document.getElementById('loader');
-const items = document.getElementById('items');
-let xhr = new XMLHttpRequest;
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
-xhr.responseType ='json';
-xhr.send();
-xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-        loader.classList.remove('loader_active');
-        const response = xhr.response.response.Value;
-        items.insertAdjacentHTML('beforeend', 
-            '<div class="item__code"></div><div class="item__value"></div><div class="item__currency"></div>');
-        
+class СurrencyRate {
+    constructor( container ) {
+        this.container = container;
+        this.loader = container.getElementById('loader');
+        this.items = container.getElementById('items');
+        this.saveArr = [];
+
+        this.checkSaves();
     }
-})
+
+    save() {
+        const jsonArr = JSON.stringify(this.saveArr);
+        localStorage.setItem('mySave', jsonArr);
+    }
+
+    addСurrencyRate(arr) {
+        for (let element of arr) {
+            this.items.insertAdjacentHTML('beforeend', 
+                '<div class="item" data-id =""><div class="item__code"></div><div class="item__value"></div><div class="item__currency">руб.</div></div>');
+            const item = this.items.lastChild;
+            item.dataset.id = arr.indexOf(element);
+            const itemCode = item.querySelector('.item__code');
+            const itemValue = item.querySelector('.item__value');
+            itemCode.innerText = element.CharCode;
+            itemValue.innerText = (+element.Value).toFixed(4);
+            this.saveArr.push(
+                {
+                    id: item.dataset.id,
+                    CharCode: itemCode.innerText,
+                    Value: itemValue.innerText
+                }
+            )
+        }
+        this.save();
+    }
+
+    getСurrencyRate() {
+        let xhr = new XMLHttpRequest;
+        xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+        xhr.responseType ='json';
+        xhr.send();
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+                this.loader.classList.remove('loader_active');
+                const valutes = Object.values(xhr.response.response.Valute);
+                this.addСurrencyRate(valutes);
+            }
+        }) 
+    }
+
+    checkSaves() {
+        if (localStorage.getItem('mySave')) {
+            const mySave = JSON.parse(localStorage.getItem('mySave'));
+            this.loader.classList.remove('loader_active');
+            this.addСurrencyRate(mySave);
+            return;
+        }
+        this.getСurrencyRate();
+    }
+}
+
+new СurrencyRate(document);
