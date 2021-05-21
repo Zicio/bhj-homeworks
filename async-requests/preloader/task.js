119818385f7f98f4b6ce2a411b8840ed4fa52c02
Бурачков Александр1 <BurachkovAA1@ssk63.ru>
2021-05-21 15:51:55 +0400

    document-structure: tooltip Updated; async-requests: preloader Solved

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,6 +1,6 @@
 'use strict'
 class СurrencyRate {
-    constructor( container ) {
+    constructor(container) {
         this.container = container;
         this.loader = container.getElementById('loader');
         this.items = container.getElementById('items');
@@ -15,22 +15,41 @@ class СurrencyRate {
     }
 
     addСurrencyRate(arr) {
-        for (let element of arr) {
-            this.items.insertAdjacentHTML('beforeend', 
-                '<div class="item" data-id =""><div class="item__code"></div><div class="item__value"></div><div class="item__currency">руб.</div></div>');
-            const item = this.items.lastChild;
-            item.dataset.id = arr.indexOf(element);
-            const itemCode = item.querySelector('.item__code');
-            const itemValue = item.querySelector('.item__value');
-            itemCode.innerText = element.CharCode;
-            itemValue.innerText = (+element.Value).toFixed(4);
+        const itemsList = this.items.querySelectorAll('.item');
+        if (itemsList.length !== arr.length) {
+            for (let element of arr) {
+                this.items.insertAdjacentHTML('beforeend', 
+                    '<div class="item" data-id =""><div class="item__code"></div><div class="item__value"></div><div class="item__currency">руб.</div></div>');
+                const item = this.items.lastChild;
+                item.dataset.id = arr.indexOf(element);
+                const itemCode = item.querySelector('.item__code');
+                const itemValue = item.querySelector('.item__value');
+                itemCode.innerText = element.CharCode;
+                itemValue.innerText = (+element.Value).toFixed(4);
+                this.saveArr.push(
+                    {
+                        id: item.dataset.id,
+                        CharCode: itemCode.innerText,
+                        Value: itemValue.innerText
+                    }
+                )
+            }
+            this.save();
+            return;
+        }
+        localStorage.clear();
+        this.saveArr = [];
+        for (let i = 0; i < itemsList.length; i++) {
+            const elementValue = itemsList[i].querySelector('.item__value');
+            const elementCode = itemsList[i].querySelector('.item__code');
+            elementValue.innerText = (arr[i].Value).toFixed(4);
             this.saveArr.push(
                 {
-                    id: item.dataset.id,
-                    CharCode: itemCode.innerText,
-                    Value: itemValue.innerText
+                    id: itemsList[i].dataset.id,
+                    CharCode: elementCode.innerText,
+                    Value: elementValue.innerText
                 }
-            )
+            )    
         }
         this.save();
     }
@@ -49,14 +68,21 @@ class СurrencyRate {
         }) 
     }
 
+    start() {
+        let timerId = setInterval(() => {
+            this.getСurrencyRate();
+        }, 10000);
+    }
+
     checkSaves() {
         if (localStorage.getItem('mySave')) {
             const mySave = JSON.parse(localStorage.getItem('mySave'));
             this.loader.classList.remove('loader_active');
             this.addСurrencyRate(mySave);
-            return;
+            this.start();
         }
         this.getСurrencyRate();
+        this.start();
     }
 }
 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,41 +1,41 @@
 'use strict'
 const hyperlinks = document.querySelectorAll('.has-tooltip');
+const newElement = document.createElement('div');
+newElement.classList.add('tooltip');
+newElement.setAttribute('style', 'position: absolute; width: max-content');
 
-const setCoordTooltip = (element, newElement) => {
-    if ((element.dataset.position).localeCompare('top') === 0) {
-        newElement.style.left = '0';
-        newElement.style.top = '-30px';
+const getPositionNewElement = (targetElement) => {
+    //newElement.removeAttribute("style");
+    const coord = targetElement.getBoundingClientRect();
+    if ((targetElement.dataset.position).localeCompare('top') === 0) {
+        newElement.style.left = coord.left +'px';
+        newElement.style.top = coord.top - 30 +'px';
         return;
     }
-    if ((element.dataset.position).localeCompare('left') === 0) {
-        newElement.style.right = '102%';
-        newElement.style.top = '-5px';
+    if ((targetElement.dataset.position).localeCompare('left') === 0) {
+        newElement.style.left = coord.left - 175 +'px';
+        newElement.style.top = coord.bottom - 25 +'px';
         return;
     }
-    if ((element.dataset.position).localeCompare('right') === 0) {
-        newElement.style.left = '102%';
-        newElement.style.top = '-5px';
+    if ((targetElement.dataset.position).localeCompare('right') === 0) {
+        newElement.style.left = coord.right + 15 +'px';
+        newElement.style.top = coord.bottom - 22 +'px';
         return;
     }
-    if ((element.dataset.position).localeCompare('bottom') === 0) {
-        newElement.style.left = '0';
+    if ((targetElement.dataset.position).localeCompare('bottom') === 0) {
+        newElement.style.left = coord.left +'px';
+        newElement.style.top = coord.bottom + 5 +'px';
         return;
     }
-}
-
-for (const element of hyperlinks) {
-    const newElement = document.createElement('div');
-    newElement.classList.add('tooltip');
-    newElement.setAttribute('style', 'position: absolute; width: max-content');
-    element.style.position = 'relative';
-    newElement.innerText = `${element.title}`;
-    element.appendChild(newElement);
-    setCoordTooltip(element, newElement);
 }
 
 document.addEventListener('click', (e) => {
+    const targetElement = e.target;
     if (e.target.classList.contains('has-tooltip')) {
         e.preventDefault();
+        newElement.innerText = `${e.target.title}`;
+        getPositionNewElement(targetElement);
+        e.target.appendChild(newElement);
         const hint = e.target.querySelector('.tooltip');
         if (e.target.querySelector('.tooltip_active') || !document.querySelector('.tooltip_active')) {
         hint.classList.toggle('tooltip_active');
